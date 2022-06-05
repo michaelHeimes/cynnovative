43a4219b2c78b70e01299b31551a2eaa6f6a9cb2
Michael Heimes <michael@cairndigitalmedia.com>
2022-06-04 21:21:29 -0400

    Convert job opening accordion to post links.

--- page-templates/page-job-openings.php
+++ page-templates/page-job-openings.php
@@ -45,7 +45,6 @@ get_header(); ?>
 				<?php endif;?>
 				
 				<section class="jobs-wrap">
-					<ul class="accordion" data-allow-all-closed="true" data-accordion>
 					<?php			
 					$args = array(  
 				        'post_type' => 'job',
@@ -61,69 +60,19 @@ get_header(); ?>
 				        
 					    while ( $loop->have_posts() ) : $loop->the_post();?>
 					    
-					    	<li class="single-job accordion-item" data-accordion-item>						
-								<a href="#" class="accordion-title cell small-12">
-									<div class="gradient-bg"></div>
-									<div class="grid-container">
-										<div class="grid-x grid-padding-x">
-											<div class="inner cell small-12">
-									        	<h2><?php print the_title();?></h2>
-												<h3><span><?php the_field('location');?></span> \ <span><?php the_field('levels');?></span></h3>
-											</div>
+				    	<div class="single-job">						
+							<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>" class="accordion-title cell small-12">
+								<div class="gradient-bg"></div>
+								<div class="grid-container">
+									<div class="grid-x grid-padding-x">
+										<div class="inner cell small-12">
+								        	<h2><?php print the_title();?></h2>
+											<h3><span><?php the_field('location');?></span> \ <span><?php the_field('levels');?></span></h3>
 										</div>
 									</div>
-								</a>
-								
-							    <div class="accordion-content" data-tab-content>
-								    <div class="grid-container">
-										<div class="grid-x grid-padding-x">
-											
-											<div class="single-section cell small-12">
-												<h4>
-													<?php the_field('company_overview_title', 'option');?>
-												</h4>
-												<div class="big-copy copy-wrap">
-													<?php the_field('company_overview_copy', 'option');?>
-												</div>
-											</div>
-											
-											<?php if( have_rows('content_sections') ):?>
-												<?php while ( have_rows('content_sections') ) : the_row();?>	
-												<div class="single-section cell small-12">
-												<?php if( have_rows('single_section') ):?>
-													<?php while ( have_rows('single_section') ) : the_row();?>	
-													<h4>
-														<?php the_sub_field('heading');?>
-														
-											        	<?php
-														$sub = get_sub_field('add_requirement_label');
-														if( $sub && in_array('yes', $sub) ):?>	
-														<br>									        	
-											        	<span>
-											        		<img class="type-cursor" src="/wp-content/themes/cynnovative/assets/images/Cynnovative_Slash_White.svg"/>
-											        		<?php the_sub_field('requirement_label');?>
-											        	</span>
-											        	<?php endif;?>
-										        	
-													</h4>
-													<div class="big-copy copy-wrap">
-														<?php the_sub_field('copy');?>
-													</div>
-													<?php endwhile;?>
-												<?php endif;?>
-												</div>
-												<?php endwhile;?>
-											<?php endif;?>
-											
-											<div class="big-copy job-footnote cell small-12">
-												<?php the_field('job_post_footnote', 'option');?>
-											</div>
-											
-										</div>
-								    </div>
-							    </div>
-
-					    	</li>
+								</div>
+							</a>
+				    	</div>
 					    	
 					    <?php
 					    endwhile;
